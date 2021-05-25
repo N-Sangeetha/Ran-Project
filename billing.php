61fad77d4d1d556e81f157460a6918b35f176021
sangeetha-cronj <sangeetha@cronj.com>
2021-05-25 10:24:01 +0530

    Initial commit

---
+++ billing.php
@@ -0,0 +1,7 @@
+<?php include('./layout/header.html'); ?>
+    
+<!-- [ Main Content ] start -->
+
+<!-- [ Main Content ] end -->
+                        
+<?php include('./layout/footer.html'); ?>
---
+++ scripts/common/common.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  // $('#BreadcrumbsDiv').load('../../layout/breadcrumbs.html')
+})
